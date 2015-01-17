929a1352c90c213d1895ebdf3c22639b086d2078
Daniel Paulus <d.paulus@gmail.com>
2015-01-17 19:41:04 +0100

    inital commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'blog_cookbook'
+maintainer       'Daniel Paulus'
+maintainer_email 'd.paulus@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures blog_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
